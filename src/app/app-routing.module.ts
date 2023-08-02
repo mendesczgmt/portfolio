639205f63147b0f152325712f3ffa7b8cf1698b4
Gabryel Mendes <gabryelmendescz@gmail.com>
2023-08-02 10:58:39 -0300

    criação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/usuario/login/login.component';
+import { CadastroComponent } from './componentes/usuario/cadastro/cadastro.component';
+import { MainComponent } from './componentes/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'loginUsuario', component: LoginComponent
+  },
+  {
+    path: 'cadastroUsuario', component: CadastroComponent
+  },
+  {
+    path: '', component: MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { MainComponent } from './componentes/main/main.component';
+import { LoginComponent } from './componentes/usuario/login/login.component';
+import { CadastroComponent } from './componentes/usuario/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    FooterComponent,
+    MainComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
